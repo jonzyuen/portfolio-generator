a9d583ababca8c3e289fbe1f21b6307e3be2d826
Jonathan Yuen <jonzyuen@gmail.com>
2021-08-07 19:25:23 -0700

    9.2

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = profileDataArr => {
-  // this 
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  };
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
 
-  console.log("================");
 
-  // is the same as 
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile("index.html", generatePage(name,github), err => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
+
+// const printProfileData = profileDataArr => {
+//   // this 
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   };
+
+//   console.log("================");
+
+//   // is the same as 
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
 
 
 // using function expression syntax
